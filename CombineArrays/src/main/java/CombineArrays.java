803beb2a351fa3d08fd79e8568cea61083bc0979
betymesfin <bethelhemteferra@gmail.com>
2023-01-26 20:30:54 +0000

    Saved progress on CombineArrays 2023-01-26 20:30:54 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,20 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int aLen = arr1.length;
+        int bLen = arr2.length;
+        int[] result = new int[aLen + bLen];
+        int i = 0;
+        for (int element : arr1) {
+            result[i] = element;
+            i++;
+        }
 
-        return null;
+        for (int element : arr2) {
+            result[i] = element;
+            i++;
+        }
+
+        return result;
     }
 }

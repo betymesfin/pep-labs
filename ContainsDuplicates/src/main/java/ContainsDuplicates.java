90d4568940d77f7302a2f1b36358379405c8b271
betymesfin <bethelhemteferra@gmail.com>
2023-01-26 19:46:28 +0000

    Saved progress on ContainsDuplicates 2023-01-26 19:46:28 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+            
+        }
         return false;
     }
 }

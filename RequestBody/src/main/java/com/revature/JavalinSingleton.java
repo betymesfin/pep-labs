a82b78e3068c02f7f9d32dc4e627aff6563bbbba
betymesfin <bethelhemteferra@gmail.com>
2023-01-31 20:26:12 +0000

    Saved progress on RequestBody 2023-01-31 20:26:12 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -18,6 +20,14 @@ public class JavalinSingleton {
          */
         app.post("/problem1", ctx -> {
                 //implement logic here
+                String jsonString = ctx.body();
+                ObjectMapper om = new ObjectMapper();
+                Song song = om.readValue(jsonString, Song.class);
+                //ctx.contentType("application/json"); 
+                song.getArtistName();
+                //String getartistName = om.writeValueAsString(song);
+                ctx.result(song.getArtistName());
+
         });
 
         /**
@@ -29,6 +39,16 @@ public class JavalinSingleton {
          */
         app.post("/problem2", ctx -> {
                //implement logic here
+               String jsonString = ctx.body();
+               ObjectMapper om = new ObjectMapper();
+               Song song = om.readValue(jsonString, Song.class);
+               ctx.contentType("application/json"); 
+
+               song.setArtistName("Beatles");
+    
+               String artistNamereturned = om.writeValueAsString(song);
+               ctx.result(artistNamereturned);
+              
         });
 
 

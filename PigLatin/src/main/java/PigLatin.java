1651ca4182817925a159516a055da81edb794a6d
betymesfin <bethelhemteferra@gmail.com>
2023-02-20 22:03:34 +0000

    Saved progress on PigLatin 2023-02-20 22:03:34 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,6 +17,7 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String piglatin=in.substring(1) + in.substring(0,1) + "ay";
+        return piglatin;
     }
 }

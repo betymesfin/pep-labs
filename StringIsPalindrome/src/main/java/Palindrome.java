157a201b2fc0fc18414b35768f568867fa52a76f
betymesfin <bethelhemteferra@gmail.com>
2023-02-21 19:03:52 +0000

    Saved progress on StringIsPalindrome 2023-02-21 19:03:52 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -13,16 +13,13 @@ public class Palindrome {
      */
     public boolean pal(String str){
         String rev = "";
- 
-        boolean ans = false;
- 
         for (int i = str.length() - 1; i >= 0; i--) {
             rev = rev + str.charAt(i);
         }
  
         if (str.equals(rev)) {
-            ans = true;
+           return true;
         }
-        return ans;
+        return false;
     }
 }

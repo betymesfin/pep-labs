6d1e2da612c5200c73259e2ec73ff5be8e288b61
betymesfin <bethelhemteferra@gmail.com>
2023-02-24 02:49:03 +0000

    Saved progress on Interfaces 2023-02-24 02:49:03 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,8 +10,25 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
+        char[] ch = new char[str.length()];
+        Map<Character,Integer> map=new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            ch[i] = str.charAt(i);
+        }
+        int max = 0; 
+        char result = ' ';
+ 
         
-        
-        return ' ';
+        for (int i = 0; i < ch.length; i++) {
+            map.put(ch[i],0);
+
+            if (max < ch[str.charAt(i)]) {
+                max = ch[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+ 
+        return result;
     }
 }
+        

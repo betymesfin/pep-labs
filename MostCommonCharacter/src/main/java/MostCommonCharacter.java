ab353854b3065df2463675b15e88a8a8ab19d11f
betymesfin <bethelhemteferra@gmail.com>
2023-02-25 00:36:19 +0000

    Saved progress on MostCommonCharacter 2023-02-25 00:36:19 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -9,23 +9,39 @@ public class MostCommonCharacter {
      */
     public char recurringChar(String str) {
           
-        int[] freq = new int[str.length()];  
+        int[] count = new int[str.length()];  
         char maxChar = str.charAt(0);  
         int i, j, max;          
         char string[] = str.toCharArray(); 
         
           
-        //Count each word in given string and store in array freq  
+          
         for(i = 0; i < string.length; i++) {  
-            freq[i] = 1;  
+            count[i] = 1;  
             for(j = i+1; j < string.length; j++) {  
                 if(string[i] == string[j] && string[i] != ' ' && string[i] != '0') {  
-                    freq[i]++;  
+                    count[i]++;  
                       
-                    //Set string[j] to 0 to avoid printing visited character  
+                      
                     string[j] = '0';   
           
     }
     }
+   }
+  
+  //Determine minimum and maximum occurring characters  
+  max = count[0];  
+  for(i = 0; i <count.length; i++) {  
+        
+      //If max is less than frequency of a character   
+      //then, store frequency in max and corresponding character in maxChar  
+      if(max < count[i]) {  
+          max = count[i];  
+          maxChar = string[i];  
+      }  
+  }  
+  return maxChar;
+}
+}
 
         
